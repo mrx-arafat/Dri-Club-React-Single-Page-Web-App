6c523548a6c6d9af2e89da764df2c1856bff164f
Easin Arafat <arafatmrx@gmail.com>
2021-10-05 08:25:18 +0600

    about modifying

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,17 +6,20 @@ const About = () => {
     <div>
       <h1>Our Overview</h1>
       <div className="about-container">
-        <div className="text-container">
+        <div className="all-container">
           <img src={img1} alt="" />
-          <h2>Affordable, Effective and Convenient Driving Lessons</h2>
-          <p className="about-p">
-            Safe Drive Driving School has designed classes with the busy student
-            in mind. We have a very flexible system of classes that allows
-            students to take the classes in any order they wish and at their own
-            pace. In some schools you have a very strict schedule for taking the
-            classes and this does not work into a lot of the current schedules
-            of kids busy lives.
-          </p>
+
+          <div className="text-container">
+            <h2>Affordable, Effective and Convenient Driving Lessons</h2>
+            <p className="about-p">
+              Safe Drive Driving School has designed classes with the busy
+              student in mind. We have a very flexible system of classes that
+              allows students to take the classes in any order they wish and at
+              their own pace. In some schools you have a very strict schedule
+              for taking the classes and this does not work into a lot of the
+              current schedules of kids busy lives.
+            </p>
+          </div>
         </div>
       </div>
     </div>
